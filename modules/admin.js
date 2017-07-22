f149a190b2609aee3f839262c6c0c350730566f0
Luke Olson <skyjedi@gmail.com>
2017-07-22 13:56:55 -0500

    Update admin.js

--- modules/admin.js
+++ modules/admin.js
@@ -1,10 +1,10 @@
 function admin(command, message, bot) {
   switch (command) {
-    case "servers":
+    case "stats":
       message.author.sendMessage(`Currently on ${bot.guilds.size} servers!`);
-      break;
-    case "users":
       message.author.sendMessage(`Currently assisting ${bot.guilds.reduce((a, b) => a + b.memberCount, 0)} users!`);
+      break;
+    Default:
       break;
   }
 }
