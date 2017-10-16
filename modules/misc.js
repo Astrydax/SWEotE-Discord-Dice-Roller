bb95aaf1dcab81c12a3e2660055f5911bd008afe
SkyJedi <skyjedi@gmail.com>
2017-10-16 08:50:59 -0500

    last of the json

--- modules/misc.js
+++ modules/misc.js
@@ -1,4 +1,5 @@
 const config = require("../config.json");
+const firebase = require('firebase');
 
 var Species =
 ["Aleena", "Anx", "Aqualish", "Arcona", "Arkanian Offshoot", "Arkanian", "Barabel", "Bardottan", "Besalisk", "Bith", "Bothan", "Caamasi", "Cathar", "Cerean", "Chadra-Fan", "Chagrian", "Chevin", "Chiss", "Clawdite", "Corellian Human", "Dashade", "Defel", "Devaronian", "Drall", "Dressellian", "Droid", "Dug", "Duros", "Elom", "Elomin", "Ewok", "Falleen", "Farghul", "Gamorrean", "Gand", "Gank", "Givin", "Gossam", "Gotal", "Gran", "Gungan", "Herglic", "Human", "Hutt", "Iktotchi", "Ishi Tib", "Ithorian", "Jawa", "Kalleran", "Kel Dor", "Klatooinian", "Kubaz", "Kyuzo", "Lannik", "Lepi", "Mandalorian Human", "Mirialan", "Mon Calamari", "Mustafarian", "Muun", "Nagai", "Nautolan", "Neimoidian", "Nikto", "Noghri", "Ortolan", "Pantoran", "Pau'an", "Polis Massan", "Quarren", "Quermian", "Rodian", "Ryn", "Sakiyan", "Sathari", "Selkath", "Selonian", "Shistavanen", "Sluissi", "Snivvian", "Squib", "Sullustan", "Talz", "Thakwaash", "Togorian", "Togruta", "Toydarians", "Trandoshan", "Twi'lek", "Ubese", "Ugnaught", "Verpine", "Weequay", "Whiphid", "Wookiee", "Xexto", "Zabrak", "Zeltron", "Zygerrian"];
@@ -42,7 +43,7 @@ function statUpdate(botStats, bot) {
     //reset dailies
     botStats.daily[stat] = 0;
   })
-  jsonfile.writeFile(`.${config.dataPath}/data/botStats.json`, botStats);
+  firebase.database().ref().child(`${bot.user.username}`).child('botStats').set(botStats);
   return botStats;
 }
 
