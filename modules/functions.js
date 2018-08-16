8d6d6c8ee8dfd4ed31f9e56c4430d7683a7652ce
SkyJedi <skyjedi@gmail.com>
2018-08-16 14:22:14 -0500

    fixed command weirdness

--- modules/functions.js
+++ modules/functions.js
@@ -9,7 +9,6 @@ function dice(sides) {
 
 async function buildPrefix(bot, message) {
 	return new Promise(async resolve => {
-		//Ignore messages sent by the bot
 		let prefix = await functions.readData(bot, message, 'prefix');
 		if (!prefix) prefix = functions.config.prefix;
 		if (message.content.includes(bot.user.id) && message.content.includes('prefix')) message.channel.send(`${bot.user.username} is using ${prefix} as the activator for this server`);
@@ -21,22 +20,26 @@ async function buildPrefix(bot, message) {
 
 function buildParams(message, prefix) {
 	let params = message.content.split(' ');
-	if (!message.content.startsWith(prefix)) {
+	if (!params[0].startsWith(prefix)) {
+		let newParams = false;
 		params.forEach((param, index) => {
-			if (param.startsWith(prefix)) params = params.slice(index);
+			if (param.startsWith(prefix)) newParams = params.slice(index);
 		});
+		if (!newParams) return;
+		params = newParams;
 	}
 
 	//remove user mentions
 	params.forEach((param, index) => {
 		if (param.includes('<') && param.includes('>')) params.splice(index, 1);
 	});
+
 	return params;
 }
 
 function buildCommand(params) {
 	//create command
-	let command = params[0].toString().slice(1);
+	let command = params[0].slice(1);
 	params = params.slice(1);
 	return [_.toLower(command), params];
 }
