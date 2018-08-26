9ed3b6799482774aa2d88a08aec8cfa0d650d0cb
SkyJedi <skyjedi@gmail.com>
2018-08-26 09:30:38 -0500

    patreon check

--- modules/functions.js
+++ modules/functions.js
@@ -72,7 +72,7 @@ function buildParams(message, prefix) {
 
 function buildCommand(params) {
 	//create command
-	if (!params[0]) return ['', params];
+	if (!params[0]) return [false, params];
 	let command = params[0].slice(1);
 	params = params.slice(1);
 	return [_.toLower(command), params];
